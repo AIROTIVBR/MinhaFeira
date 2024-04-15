549f4e04f63709b973114a60777a6d066dfabfba
Igor Gabriel <rogiborgessouza@outlook.com>
2024-04-15 18:28:00 -0300

    Inicio da configuração do nativewind e arquitetura

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// import { colors } from './src/assets/constants/colors' import colors
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    //insert colors imported
+    extend: {},
+  },
+  plugins: [],
+}
