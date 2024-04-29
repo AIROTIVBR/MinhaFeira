f1edcd2835b9c784fff9b272c28905d5aef11b31
Igor Gabriel <rogiborgessouza@outlook.com>
2024-04-29 15:57:00 -0300

    fix: adjust initial config

---
+++ src/app/_layout.tsx
@@ -0,0 +1,7 @@
+import '@/assets/constants/global.css'
+
+import { Slot } from 'expo-router'
+
+export default function Layout() {
+    return <Slot/>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     //insert colors imported
